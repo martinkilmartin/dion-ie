9b2b7534263489a0560d96addb2b419079034eed
martinkilmartin <martinkilmartin@gmail.com>
2022-01-04 12:37:45 -0500

    Customise to díon.ie

--- src/components/SVG/index.tsx
+++ src/components/SVG/index.tsx
@@ -1,3 +1,3 @@
-export { default as SionnachSolutionsLogo } from './SionnachSolutionsLogo'
+export { default as DionLogo } from './DionLogo'
 export { default as Sun } from './Sun'
 export { default as Moon } from './Moon'
